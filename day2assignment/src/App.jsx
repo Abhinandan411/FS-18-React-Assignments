b39665784ddbcd11b7414a4430f0d629129283d1
Abhinandan411 <abhinandankumar6204@gmail.com>
2024-05-29 09:39:39 +0530

    update something

--- day2assignment/src/App.jsx
+++ day2assignment/src/App.jsx
@@ -6,13 +6,13 @@ function App() {
       <div className="main-container">
         <h1 className="heading">Learn Web Development</h1>
         <div className="first-div">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus illum, consequatur non quia et maxime esse minus numquam aliquam, totam ut tempora reiciendis laudantium aut veritatis tenetur. Mollitia, enim laudantium.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus illum, consequatur non quia <a href="">somthing more </a>et maxime esse minus numquam aliquam, totam ut tempora reiciendis laudantium aut veritatis tenetur. Mollitia, enim laudantium.</p>
         </div>
         <div className="second-div">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur velit blanditiis saepe dolorum placeat nemo tenetur nulla natus. Beatae repellendus quae quisquam ipsa possimus similique sit soluta iure et nemo?</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. <a href="">somthing more </a>Consequatur velit blanditiis saepe dolorum placeat nemo tenetur nulla natus. Beatae repellendus quae quisquam ipsa possimus similique sit soluta iure et nemo?</p>
         </div>
         <div className="third-div">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, soluta corporis? Architecto laudantium, eum corporis repellendus maioresnatus facereex sint molestiae doloremque aliquam! Maiores officia soluta ut neque.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, soluta <a href="">somthing more </a> corporis? Architecto laudantium, eum corporis repellendus maioresnatus facereex sint molestiae doloremque aliquam! Maiores officia soluta ut neque.</p>
         </div>
         
       </div>
