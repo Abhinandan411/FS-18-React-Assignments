576531312e40a9a6c5e344ed52af65e4452df950
Abhinandan411 <abhinandankumar6204@gmail.com>
2024-05-30 19:08:13 +0530

    added geekfood assignment

---
+++ Geekfood/src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Section1 from './components/Section1'
+import Testimonial from './components/Testimonial'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <Navbar/>
+      <Hero/>
+      <Section1/>
+      <Testimonial/>
+      <Footer/>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
